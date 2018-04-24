faa1e7b156ee6986bc972ceed9dcfda8ed523214
Administrador <Administrador@172.16.15.124>
2018-04-24 11:25:59 -0600

    Parte 1 matrices

---
+++ src/matricesymodularidadsis16/MatricesYModularidadSis16.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matricesymodularidadsis16;
+
+/**Nombre de la clase:MatricesYModularidadSis16.
+ * version: beta.
+ * fecha: 2-04-2018.
+ *copyright: Si.
+ * @author Carlos Lopez.
+ */
+public class MatricesYModularidadSis16 {
+
+ 
+    public static void main(String[] args) {
+        
+    }
+    
+}
